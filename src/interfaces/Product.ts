7f65b038a732d1033b4e86b2eeb6e1add26ab0d8
Beterraba <ls6182315@gmail.com>
2024-09-25 17:27:59 -0300

    remove unsed attributes

--- src/interfaces/Product.ts
+++ src/interfaces/Product.ts
@@ -1,15 +1,8 @@
-interface Product {
+export interface Product {
     title: string,
     description: string,
     price: number,
     imgUrl: string,
-    memoryType: Memory
-    swatch: string[],
     rating: number,
     review: number
-}
-
-enum Memory {
-    HDD,
-    SSD
 }
